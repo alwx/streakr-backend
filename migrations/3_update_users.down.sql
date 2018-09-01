ce59c6987351fe9aa8b4a607fd2c667ac624b348
Aleksandr Pantiukhov <alwxndr@gmail.com>
2018-09-01 13:07:00 +0200

    Fixes

---
+++ migrations/3_update_users.down.sql
@@ -0,0 +1,7 @@
+ALTER TABLE users ADD COLUMN username TEXT NOT NULL DEFAULT "username";
+ALTER TABLE users DROP COLUMN api_key;
+ALTER TABLE users DROP COLUMN public_key;
+ALTER TABLE users DROP COLUMN private_key;
+ALTER TABLE users DROP COLUMN user_token;
+ALTER TABLE users DROP COLUMN display_name;
+ALTER TABLE users DROP COLUMN user_person_id;
